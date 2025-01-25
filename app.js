b51a3d967574653a9e0c2d7b4255d1379a1c2760
Tharun <tharungowda30115@gmail.com>
2025-01-25 22:17:46 +0530

    completed

---
+++ app.js
@@ -0,0 +1,22 @@
+const genBtn = document.querySelector('.btn1')
+const copyBtn = document.querySelector('.btn2')
+let password ;
+genBtn.addEventListener('click', () => genPassword())
+copyBtn.addEventListener('click', () => copyPassword())
+
+function genPassword() {
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*_.ABCD"
+    passwordLength = 9
+    password =""
+    for(let i =0; i<=passwordLength;i++){
+        let randomNumber = Math.floor(Math.random() * chars.length)
+        password +=chars.substring(randomNumber,randomNumber+1)
+    }
+    document.getElementById("password").value = password
+}
+
+function copyPassword() {
+    let copyText=document.getElementById("password")
+    copyText.select()
+    document.execCommand('copy')
+}
